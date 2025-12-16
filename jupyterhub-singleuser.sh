f92a605a43ebc4c7c440c391e64759039b61e0dd
Jacob Renn <jacob.renn4@gmail.com>
2025-12-16 13:20:30 -0500

    updating
    Signed-off-by: Jacob Renn <jacob.renn4@gmail.com>

--- jupyterhub-singleuser.sh
+++ jupyterhub-singleuser.sh
@@ -4,5 +4,6 @@ source /etc/profile.d/dask.sh
 source /etc/profile.d/mlflow.sh
 source /etc/profile.d/api.sh
 source /etc/profile.d/ollama.sh
+source /etc/profile.d/s3.sh
 
 exec jupyterhub-singleuser
